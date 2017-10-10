f5331315c9c08c3694aa35c77e0674509c46bc51
Mukund K.V <31546824+mveerara@users.noreply.github.com>
2017-10-10 10:53:19 +0530

    Cachematrix made some changes
    Made some changes to Assignment cahchematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,8 +6,8 @@
 ## which is really a list containing a function to
 ## 1.	set the value of the vector
 ## 2.	get the value of the vector
-## 3.	set the value of the mean
-## 4.	get the value of the mean
+## 3.	set the value of the inverse
+## 4.	get the value of the inverse
 
 
 makeCacheMatrix <- function(x = matrix()) {
